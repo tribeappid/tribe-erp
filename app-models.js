b88908061bfd7f9ba7740e542a9747db6c1601aa
tribeappid <34232614+tribeappid@users.noreply.github.com>
2018-03-19 22:05:53 +0800

    - Add GET/POST /admin/getBranch /admin/addBranch

--- app-models.js
+++ app-models.js
@@ -1,5 +1,6 @@
 mentity = require('./models/entity');
 menterprise = require('./models/enterprise');
+mbranch = require('./models/branch');
 mevent = require('./models/event');
 mcloud_access = require('./models/cloud_access');
 mlookup_value = require('./models/lookup_value');
--- routes/admin/index.js
+++ routes/admin/index.js
@@ -46,4 +46,48 @@ router.get('/getEnterprise', function(req, res, callback){
     //});
 });
 
+router.post('/addBranch', function(req, res, callback){
+    //authorizationHelper.authorize(req, res, [__ENTERPRISE_ADMIN_AUTH, __ENTERPRISE_SYS_ADMIN_AUTH, __ADMIN_AUTH, __SYS_ADMIN_AUTH], null, null, override, function(authorized){
+        if (1) {
+            if (req.body.Name && req.body.EnterpriseId)
+            {
+                var addBranchReq = _.clone(req);             
+                enterpriseController.addBranch(addBranchReq, res, true, function(err, data){
+                    if (!err){
+                        apiHelper.addRes(req, res, null, data, callback);
+                    }else{
+                        apiHelper.addRes(req, res, err, null, callback);
+                    }
+                });
+            } else {
+                apiHelper.apiResponse(req, res, true, 500, "Missing Parameter", null, null, null, null);
+            }
+        } else {
+            apiHelper.apiResponse(req, res, true, 401, "Not Authorized", null, null, null, null);
+        }
+    //});
+});
+
+router.get('/getBranch', function(req, res, callback){
+    //authorizationHelper.authorize(req, res, [__ENTERPRISE_ADMIN_AUTH, __ENTERPRISE_SYS_ADMIN_AUTH, __ADMIN_AUTH, __SYS_ADMIN_AUTH], null, null, override, function(authorized){
+        if (1) {
+            if (req.query.EnterpriseId)
+            {
+                var getBranchReq = _.clone(req);
+                enterpriseController.getBranch(getBranchReq, res, true, function(resGetEntityErr, resGetEntity, resGetEntityRowsReturned, resGetEntityTotalRows){
+                    if (!resGetEntityErr){
+                        apiHelper.apiRes(req, res, null, null, resGetEntity, null, null, null);
+                    }else{
+                        apiHelper.apiRes(req, res, null, null, null, resGetEntityErr, null, null);
+                    }
+                });
+            } else {
+                apiHelper.apiResponse(req, res, true, 500, "Missing Parameter", null, null, null, null);
+            }
+        } else {
+            apiHelper.apiResponse(req, res, true, 401, "Not Authorized", null, null, null, null);
+        }
+    //});
+});
+
 module.exports = router;
