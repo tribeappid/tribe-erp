81e301d66131866f3de6d27bee7414d8e6c4e48d
tribeappid <34232614+tribeappid@users.noreply.github.com>
2018-03-20 07:34:26 +0700

    Add branch models

---
+++ models/branch.js
@@ -0,0 +1,33 @@
+/*
+ * AST Enterprise Model
+ *
+ * Copyright 2017 Tribe App Indonesia
+ *
+ * Version 1.0.0
+ * Author: Hendra
+ * 
+ * Change Log
+ * ----------
+ *
+ */
+
+var Schema = mongoose.Schema;
+
+branchSchema = new Schema({
+    _id : { type : String }
+    , name : { type : String, default : null }
+    , description : {type: String, default : null}
+    , create_date : {type: Date, default : null}
+    , last_update : {type: Date, default : null}
+    , enterprise : { type: String, ref: 'enterprise'}
+    , entities : [{
+        type: String
+        , ref : 'entity'
+    }]
+    , medias : [{
+        type: Schema.Types.ObjectId
+        , ref : 'media'
+    }]    
+});
+
+mongoose.model('branch', branchSchema, 'branch');
