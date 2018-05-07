95a8957ebca7d15dd8af259e6b00dc254cfcf0e2
HS <chintiongng@gmail.com>
2018-05-07 09:55:07 +0700

    Hendra - Fix incorrect images path

--- client/src/components/loading_screen.jsx
+++ client/src/components/loading_screen.jsx
@@ -6,7 +6,7 @@ export default class LoadingScreen extends Component{
             <div>
                 <div id="loadingScreen" hidden={true} style={`width: 100%;height:100%;position:fixed;opacity:0.7;background:#000;z-index:99;text-align:center;`}>
                     <div style={`margin-top:20%;`}/>
-                    <img style={`width:190px;position:relative;z-index:100;`} src="http://localhost:3000/images/rolling.gif"/>
+                    <img style={`width:190px;position:relative;z-index:100;`} src="/images/rolling.gif"/>
                 </div>
             </div>
         )
